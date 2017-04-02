1ceb955406bcf14bde0e43cb24bdde4001d08f71
Yvette <lauyatting9598@gmail.com>
2017-04-02 16:03:07 +0800

    认证详情存在问题

--- js/authenticationInfor.js
+++ js/authenticationInfor.js
@@ -1,15 +1,18 @@
-var modal= document.getElementById('myModal');
-var img ="document".getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-	modal.style.display = "block";
-	modalImg.src = this.src;
-	modalImg.alt = this.alt;
-	captionText.innerHTML = this.alt;
-}
+$(document).ready(function(){
+	var modal= document.getElementById('myModal');
+	var img =document.getElementById("authenTec");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		modalImg.alt = this.alt;
+		captionText.innerHTML = this.alt;
+	}
 
-var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-	modal.style.display = "none";
-}
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+})
